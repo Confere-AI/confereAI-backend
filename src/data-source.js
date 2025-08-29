94c53bb172e9ce904a4f18892ff46bd71ce814dc
jp066 <jpedro030506@gmail.com>
2025-08-29 17:38:24 -0300

    ajeitando config2

--- src/data-source.js
+++ src/data-source.js
@@ -4,8 +4,8 @@ const User = require("./entity/User");
 
 const AppDataSource = new DataSource({
   type: "postgres",
-  host: process.env.DB_HOST || "localhost",
-  port: 5432,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
